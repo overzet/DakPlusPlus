475533532b5deb0c34a6f5b227d770800360b3cd
overzet <overzet@gmail.com>
2018-09-10 15:38:34 +0200

    Employee Create, Update and Remove works commit

---
+++ src/main/java/daos/RemoveEmployee.java
@@ -0,0 +1,33 @@
+package daos;
+
+import entities.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class RemoveEmployee {
+
+        public static void main(String[] args) {
+
+            EntityManagerFactory emf = Persistence
+                    .createEntityManagerFactory("D++");
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+
+            tx.begin();
+            /* Remove a User entity */
+            Employee removeEmployee = em.find(Employee.class, 19);
+            em.remove(removeEmployee);
+
+            tx.commit();
+
+            em.close();
+            emf.close();
+            System.out.println("Employee removed.");
+        }
+
+    }
+
+
